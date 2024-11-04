3dee54299af67060b5f521ea5bc68b8a0841a530
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-11-04 21:27:45 +0530

    first commit

---
+++ src/main/java/com/JobAssist/app/JobAssistApplication.java
@@ -0,0 +1,13 @@
+package com.JobAssist.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JobAssist/app/JobAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobAssist.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
